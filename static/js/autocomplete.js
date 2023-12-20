925c99efc9d45e38111b7da2d6a8f94fb4dc43bc
Pablosarti7 <paulsarti7@gmail.com>
2023-12-20 16:41:38 -0500

    Error fix

--- static/js/autocomplete.js
+++ static/js/autocomplete.js
@@ -1,34 +1,43 @@
 var inputElement = document.getElementById("textbox");
 var resultBox = document.querySelector('.result-box');
 
+let delayTimer = null;
+
 inputElement.addEventListener("keyup", function (event) {
     var textBoxValue = inputElement.value;
 
     const query = encodeURIComponent(textBoxValue);
-    
-    fetch(`http://127.0.0.1:5000/search-suggestions?q=${query}`)
-        .then(response => response.json())
-        .then(data => {
-            
-            let result = [];
-            let input =  inputElement.value;
-            
-            if (input.length) {
-                result = data['suggestions'].filter((keyword) => {
-                    return keyword.toLowerCase().includes(input.toLowerCase());
-                })
-            }
-            console.log(result);
-            display(result);
-        })
-        .catch(error => {
-            console.error("Error:", error);
-        });
+
+    clearTimeout(delayTimer);
+
+    delayTimer = setTimeout(function() {
+
+        fetch(`https://barcode-reader-web-app.onrender.com/search-suggestions?q=${query}`)
+            .then(response => response.json())
+            .then(data => {
+
+                let result = [];
+                let input = inputElement.value;
+
+                if (input.length) {
+                    result = data['suggestions'].filter((keyword) => {
+                        return keyword.toLowerCase().includes(input.toLowerCase());
+                    })
+                }
+                console.log(result);
+                display(result);
+            })
+            .catch(error => {
+                console.error("Error:", error);
+            });
+
+    }, 2000);
+
 });
 
 // displaying the suggestion on the result-box div
 function display(result) {
-    const content = result.map((list)=>{
+    const content = result.map((list) => {
         return "<div onclick=selectInput(this)>" + list + "</div>";
     });
     resultBox.innerHTML = content.join('');
