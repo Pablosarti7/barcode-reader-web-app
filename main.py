ef0aff14f8ed183ba36f3809418e54bca3f07e79
Pablosarti7 <paulsarti7@gmail.com>
2024-06-23 23:58:33 -0400

    Adding users register and login, changes to the homepage.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for, flash
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Email, EqualTo
 from openfoodfacts_api import get_product_info
 from ingredients_api import get_all_ingredients, get_ingredient, add_ingredient
-# from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
 from openai_api import get_response
 from flask_caching import Cache
 from thefuzz import process
@@ -17,14 +19,43 @@ app.secret_key = os.urandom(24)
 
 cache = Cache(app, config={'CACHE_TYPE': 'SimpleCache'})
 
-# login_manager = LoginManager()
-# login_manager.init_app(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
-# @login_manager.user_loader
-# def load_user(user_id):
-#     return User.query.get(int(user_id))
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(user_id)
 
+
+# your database
+class Users(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+
+# with app.app_context():
+#     db.create_all()
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()], render_kw={"placeholder": "Enter Email"})
+    password = PasswordField('Password', validators=[DataRequired()], render_kw={"placeholder": "Enter Password"})
+    submit = SubmitField('Login')
+
+class RegistrationForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()], render_kw={"placeholder": "Enter Name"})
+    email = StringField('Email', validators=[DataRequired(), Email()], render_kw={"placeholder": "Enter Email"})
+    password = PasswordField('Password', validators=[DataRequired()], render_kw={"placeholder": "Enter Password"})
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')], render_kw={"placeholder": "Re-enter Password"})
+    submit = SubmitField('Register')
+
+# your flask forms
 class SearchBarcode(FlaskForm):
     ingredients = StringField('Search', validators=[DataRequired()], render_kw={
                               "placeholder": "Enter barcode here"})
@@ -126,7 +157,7 @@ def home():
         else:
             return 'Sorry product not found.'
 
-    return render_template('index.html', form=form)
+    return render_template('index.html', form=form, logged_in=current_user.is_authenticated)
 
 # search page is where users can search for specific ingredients
 @app.route('/search', methods=['GET', 'POST'])
@@ -139,7 +170,7 @@ def search():
         ingredient_from_db = get_ingredient(searched_ingredient)
         return render_template('search.html', form=form, ingredient_info=ingredient_from_db)
 
-    return render_template('search.html', form=form)
+    return render_template('search.html', form=form, logged_in=current_user.is_authenticated)
 
 # talking to the javascript code sending requests to this url
 @app.route('/search-suggestions')
@@ -155,17 +186,79 @@ def search_suggestions():
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', logged_in=current_user.is_authenticated)
 
 
 @app.route('/settings')
 def settings():
-    return render_template('settings.html')
+    subsection = request.args.get('section', 'main')
+    return render_template('settings.html', subsection=subsection, logged_in=current_user.is_authenticated)
 
 
-@app.route('/profile')
-def profile():
-    return render_template('profile.html')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    # add database
+    
+    if form.validate_on_submit():
+        # Perform login logic here
+        # flash('Login successful!', 'success')
+
+        email = form.email.data
+        password = form.password.data
+
+        result = db.session.execute(
+            db.select(Users).where(Users.email == email))
+        user = result.scalar()
+
+        if not user:
+            flash("That email does not exist, please try again.")
+            return redirect(url_for('login'))
+        elif not check_password_hash(user.password, password):
+            flash('Password incorrect, please try again.')
+            return redirect(url_for('login'))
+        else:
+            login_user(user)
+            return redirect(url_for('settings'))
+
+    return render_template('login.html', form=form, logged_in=current_user.is_authenticated)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        
+        if Users.query.filter_by(email=form.email.data).first():
+            #User already exists
+            flash("You've already signed up with that email, log in instead!")
+            return redirect(url_for('login'))
+
+        hash_and_salted_password = generate_password_hash(
+            form.password.data,
+            method='pbkdf2:sha256',
+            salt_length=8
+        )
+        new_user = Users(
+            email=form.email.data,
+            name=form.name.data,
+            password=hash_and_salted_password,
+        )
+        
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user)
+
+        return redirect(url_for("settings"))
+
+    return render_template("register.html", form=form, logged_in=current_user.is_authenticated)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
