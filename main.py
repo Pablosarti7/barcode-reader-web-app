edf3890f36cd8b3e9ca9194713b5d3ae9ac94a82
Pablosarti7 <paulsarti7@gmail.com>
2024-07-11 13:18:48 -0400

    forgot to take out print statements

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ from flask_caching import Cache
 from thefuzz import process
 import os
 import re
-import pprint
 
 
 app = Flask(__name__)
@@ -125,29 +124,24 @@ def home():
         if 'product' in ingredients:
             # accessing the key called product 
             product_info = ingredients['product']
-            print(product_info)
             # nutrients data extraction from the api
             # TODO do we need this strings of text after or can we find another method
             nutrients = product_info.get('nutriments', {})
-            print(nutrients)
             # nutriscore data extraction from the api
             nutriscore = product_info.get('nutriscore', {})
-            pprint.pprint(nutriscore)
             # ingredients percentages extraction from the api
             ingredients_percentages = product_info.get('ingredients', {})
-            print(ingredients_percentages)
             # original data extraction name and ingredients
             name = product_info.get('product_name', 'Sorry no name was found.')
             ingredients = product_info.get('ingredients_text_en', 'Sorry no ingredients where found.')
-            print(name)
-            print(ingredients)
+
 
             final_list = clean_ingredients(ingredients)
 
 
             # Ingredients that we in out database go in one list and non existing one ask chat gpt
             openai_list, database_list = [], []
-            
+
             for final_ingredient in final_list:
                 # Get the ingredient from your database
                 single_ingredient = get_ingredient(final_ingredient)
@@ -168,7 +162,6 @@ def home():
                 add_ingredient(response_list)
 
             complete_list = database_list + response_list
-            print(complete_list)
 
             return render_template('index.html', form=form, name=name, ingredients_list=complete_list, nutritional_information=nutrients, nutriscore=nutriscore, ingredients_percentage=ingredients_percentages)
         else:
