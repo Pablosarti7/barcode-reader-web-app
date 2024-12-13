6a2ba00621cfc05153c79c8032af75c1c1d2d032
Pablosarti7 <paulsarti7@gmail.com>
2024-12-12 21:13:29 -0500

    Refactoring for safety and production

--- main.py
+++ main.py
@@ -494,5 +494,5 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=False)
