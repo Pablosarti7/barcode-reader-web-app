5c376e47a738224b0be9e5cdda4a15fbd85b6276
Pablosarti7 <paulsarti7@gmail.com>
2025-01-19 21:12:52 -0500

    Fixes the df declaration error and some UX issues

--- food_recommender.py
+++ food_recommender.py
@@ -6,41 +6,49 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 
-def get_similarity_matrix():
+def create_products_df():
     data = get_all_products()
 
     # Check if the call returned an error
     if isinstance(data, dict) and "error" in data:
-        print(f"Error: {data['error']}")
-    else:
-        # Convert to DataFrame
-        try:
-            products_df = pd.DataFrame(data)  # Assuming the data is a list of dictionaries
-        except ValueError as e:
-            print(f"Error converting data to DataFrame: {e}")
+        return None
 
-        # Combine ingredients into a single string for each food item
-        ingredient_corpus = products_df['ingredients']
+    # Convert to DataFrame
+    try:
+        # Assuming the data is a list of dictionaries
+        products_df = pd.DataFrame(data)
+    except ValueError as e:
+        return None
 
-        # Initialize TF-IDF Vectorizer
-        vectorizer = TfidfVectorizer()
+    return products_df
 
-        # Fit and transform the ingredient lists
-        tfidf_matrix = vectorizer.fit_transform(ingredient_corpus)
 
-        # Compute cosine similarity between all food items
-        cosine_sim = cosine_similarity(tfidf_matrix)
+def get_similarity_matrix():
 
-        # Display the similarity matrix
-        similarity_df = pd.DataFrame(cosine_sim, index=products_df['name'], columns=products_df['name'])
-        return products_df, similarity_df
+    products_df = create_products_df()
 
-products_df, similarity_df = get_similarity_matrix()
+    # Combine ingredients into a single string for each food item
+    ingredient_corpus = products_df['ingredients']
+
+    # Initialize TF-IDF Vectorizer
+    vectorizer = TfidfVectorizer()
+
+    # Fit and transform the ingredient lists
+    tfidf_matrix = vectorizer.fit_transform(ingredient_corpus)
+
+    # Compute cosine similarity between all food items
+    cosine_sim = cosine_similarity(tfidf_matrix)
+
+    # Display the similarity matrix
+    similarity_df = pd.DataFrame(
+        cosine_sim, index=products_df['name'], columns=products_df['name'])
+    return products_df, similarity_df
+
 
 health_rating_to_score = {
-'healthy': 2,
-'neutral': 1,
-'unhealthy': -1
+    'healthy': 2,
+    'neutral': 1,
+    'unhealthy': -1
 }
 
 
@@ -48,18 +56,19 @@ def get_health_scores_from_db():
     ingredients_data = get_all_ingredients()
     # Check if the call returned an error
     if isinstance(ingredients_data, dict) and "error" in ingredients_data:
-        print(f"Error: {ingredients_data['error']}")
+        return None
     else:
-            # Convert to DataFrame
+        # Convert to DataFrame
         try:
             ingredients_df = pd.DataFrame(ingredients_data['ingredients'])
-            
-        except ValueError as e:
-            print(f"Error converting data to DataFrame: {e}")
 
-    
+        except ValueError as e:
+            return None
+
     # Exclude unwanted names first
-    excluded_items = ['Sugar', 'Corn Syrup', 'Cane Sugar', 'Pure Cane Sugar', 'Coffee Caramelized Sugar', 'Corn']
+    excluded_items = ['Sugar', 'Corn Syrup', 'Cane Sugar',
+                      'Pure Cane Sugar', 'Coffee Caramelized Sugar', 'Corn']
+
     filtered_df = ingredients_df[~ingredients_df['name'].isin(excluded_items)]
 
     # Filter for healthy, neutral, and unhealthy ratings
@@ -70,11 +79,10 @@ def get_health_scores_from_db():
     ]
 
     # Map health ratings to scores
-    filtered_df['health_score'] = filtered_df['rating'].map(health_rating_to_score)
+    filtered_df['health_score'] = filtered_df['rating'].map(
+        health_rating_to_score)
+
     return filtered_df.set_index('name')['health_score']
-
-
-health_scores = get_health_scores_from_db()
 
 
 def calculate_health_score(ingredients, health_scores):
@@ -85,27 +93,33 @@ def calculate_health_score(ingredients, health_scores):
         score += health_scores[ingredient]
     return score
 
-# Apply to dataset
-products_df['health_score'] = products_df['ingredients'].apply(lambda x: calculate_health_score(x, health_scores))
-
 
 def recommend_healthier_food(item_name, top_n=3):
-    # Get the category of the scanned item
-    
+    # Get fresh data each time the function is called
+    products_df, similarity_df = get_similarity_matrix()
+    health_scores = get_health_scores_from_db()
+
+    # Apply to dataset
+    products_df['health_score'] = products_df['ingredients'].apply(
+        lambda x: calculate_health_score(x, health_scores))
+
     item_row = products_df[products_df['name'] == item_name].iloc[0]
-    
+
     item_score = item_row['health_score']
     item_category = item_row['category'].lower()
+
+    # Check if there is NaN values and set to 0 if true
     if pd.isna(item_score):
         item_score = 0
 
     # Filter for healthier items in the same category
-    healthier_items = products_df[(products_df['health_score'] > item_score) & (products_df['category'].str.lower() == item_category)]
+    healthier_items = products_df[(products_df['health_score'] > item_score) & (
+        products_df['category'].str.lower() == item_category)]
 
     # Compute similarity scores
     sim_scores = similarity_df[item_name].loc[healthier_items['name']]
 
     # Sort and return top recommendations
     recommended_items = sim_scores.sort_values(ascending=False).iloc[:top_n]
-    
+
     return recommended_items.index.tolist()
--- main.py
+++ main.py
@@ -39,6 +39,7 @@ app.config['REMEMBER_COOKIE_SECURE'] = True
 app.config['SESSION_COOKIE_HTTPONLY'] = True
 # OAuth configurations
 app.config['GOOGLE_CLIENT_ID'] = os.environ.get("CLIENT_ID")
+
 app.config['GOOGLE_CLIENT_SECRET'] = os.environ.get("CLIENT_SECRET")
 app.config['GOOGLE_DISCOVERY_URL'] = (
     'https://accounts.google.com/.well-known/openid-configuration'
